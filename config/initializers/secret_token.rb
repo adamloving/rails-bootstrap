c37eac81cb896f0b3a5a8b45f7be9fab8b54292d
Adam <adamloving@gmail.com>
2011-09-23 00:32:36 -0700

    hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBootstrap::Application.config.secret_token = 'dd3fc64da2cb0528bd8fb54cc8c9eebbf4032e0243aa5cb4fc66f6154d3a202592a7020367da3ff1279542c55c497af8d847f7a9c9b5489cd3a668b5a94bc6d1'
