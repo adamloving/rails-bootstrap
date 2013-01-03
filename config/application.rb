4461070138e5ed6b78b9b8afdd9c90131dfaa9eb
Adam Loving <adamloving@gmail.com>
2013-01-03 10:30:32 -0800

    prefer sass

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module RailsBootstrap
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.sass.preferred_syntax = :sass
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
