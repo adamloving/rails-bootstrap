931abc028dcfbaab20449a8bedc7299d21a3a003
Adam Loving <adamloving@gmail.com>
2013-02-26 15:10:59 -0800

    Heroku not ready for ruby 2.0.0 yet

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-ruby '2.0.0'
 gem 'rails' #, :git => 'git://github.com/rails/rails.git'
 gem 'foreman'
 gem 'thin'
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 bundle exec rake assets:precompile
 git add public/assets/
-git add public/javascripts/
 git commit -m "deploy"
 git push
 git push heroku master
