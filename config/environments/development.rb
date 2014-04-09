19e498be9c3ab21daf9f555bc31ae8ce04be4f0d
Adam Loving <adamloving@gmail.com>
2014-04-08 20:33:13 -0700

    devise mailer config
    not used yet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ RailsBootstrap::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # for devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
