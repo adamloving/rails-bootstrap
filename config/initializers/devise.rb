801b470f311dcb86c497a899063fa561cce5d42c
Adam Loving <github@adamloving.com>
2016-05-12 09:35:10 -0700

    add missing omniauth keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -195,6 +195,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  config.omniauth :twitter,  ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
